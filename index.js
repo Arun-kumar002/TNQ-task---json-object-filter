63ac37815e6694d28b0f3c157745e707cfb06824
Arun-kumar002 <arunsubramanian002@gmail.com>
2022-10-19 15:01:07 +0530

    task completed

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict"
+import jsondata from './data.json' assert {type: "json"}
+let data = document.getElementById('filter')
+let tbody = document.getElementById('tbody')
+// listeners stats
+data.addEventListener('change', (e) => {
+    tbody.innerHTML = ''
+    filters(e.target.value)
+})
+// filter logic starts here
+function filters(status) {
+    let output = jsondata.filter((option) => (option.status === status))
+    output.forEach((obj) => (
+        tbody.innerHTML += `<tr>
+            <td>${obj.capsule_serial}</td>
+            <td>${obj.capsule_id}</td>
+            <td>${obj.status}</td>
+            <td>${obj.original_launch_unix}</td>
+            <td>${obj.landings}</td>
+            <td>${obj.type}</td>
+            <td>${obj.details}</td>
+            <td>${obj.reuse_count}</td>
+           <td>${obj.missions.length}</td>
+        </tr>`
+    ))
+}
